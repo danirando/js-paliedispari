1d419356e27e9def91e8a179a5178ed326deaf2d
danirando <danielerando@tiscali.it>
2025-04-07 14:26:06 +0200

    add comments

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -1,4 +1,4 @@
-// Palidroma
+// Palindroma
 
 // Chiedere all’utente di inserire una parola
 
@@ -7,40 +7,23 @@ let palindrome = true;
 // Creare una funzione per capire se la parola inserita è palindroma
 
 function isPalindrome() {
+  // chiedo una parola all'user
   const word = prompt("Inserisci una parola");
+  // di default è palindroma
   let palindrome = true;
-
+  // ciclo ripetuto tanto quanto la lunghezza della parola fratto due per spezzare la parola in due meta che devono essere uguali
   for (let i = 0; i < word.length / 2; i++) {
+    // confronto ogni lettera della parola per ogni ultima lettera andando a ritroso
     if (word[i] !== word[word.length - 1 - i]) {
+      // se sono diverse non è un palidromo
       palindrome = false;
       return alert("La parola non è palindroma");
     }
   }
+  //  se sono uguali si esce dal ciclo for, il valore palindrome è di default true
   if (palindrome) {
     alert("La parola è palindroma");
   }
 }
 
 isPalindrome();
-
-// function controllaPalindroma() {
-//   const parola = prompt("Inserisci una parola:")
-//     .toLowerCase()
-//     .replace(/\s/g, "");
-//   let èPalindroma = true;
-
-//   for (let i = 0; i < parola.length / 2; i++) {
-//     if (parola[i] !== parola[parola.length - 1 - i]) {
-//       èPalindroma = false;
-//       break;
-//     }
-//   }
-
-//   if (èPalindroma) {
-//     alert("La parola è palindroma!");
-//   } else {
-//     alert("La parola NON è palindroma.");
-//   }
-// }
-
-// controllaPalindroma();
